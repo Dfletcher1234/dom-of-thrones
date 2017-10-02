20e725b3e4b5d198c5edf5916cf3d567df491b33
David FletcherE <dtsfletcher@gmail.com>
2017-10-02 15:01:36 -0400

    done ish

---
+++ js/scene2.js
@@ -0,0 +1,43 @@
+function scene2() {
+console.log('Executing scene 2!')
+  // Setup Stage
+
+stage = document.querySelector('#stage')
+
+//document.querySelector('#sets')
+castle = document.querySelector('#castle')
+stageImg = stage.querySelector('#stage-img')
+castleImgSrc = castle.getAttribute('src')
+stageImg.setAttribute('src', castleImgSrc)
+  // Setup Cast
+var nedContainer = <div style="position: absolute; width: 10%; height: 37%; top: 55%; left: 25%; border: medium none; background: rgba(0, 0, 0, 0) none repeat scroll 0% 0%;">
+
+var ned = <img id="ned" title="#ned" src="img/cast/ned.jpg" style="border: medium none; width: 70%; position: absolute; top: 0px;">
+
+var leather = <img id="leather-armour" title="#leather-armour" src="img/wardrobe/leather-armour.jpg" style="border: medium none; position: absolute; bottom: 0px;">
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
